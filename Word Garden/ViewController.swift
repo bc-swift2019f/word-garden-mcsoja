9449a18810bace8862a40871123dc0a182b43d1c
Molly Soja <sojam@bc.edu>
2019-09-03 21:31:37 -0400

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Molly Soja on 9/3/19.
+//  Copyright © 2019 Molly Soja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
